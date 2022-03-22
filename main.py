5613aca5a67168f664a10cc2e3ddf126e154b739
Max Solovov <solovov@yandexlyceum.ru>
2022-03-22 09:19:49 +0300

    acer comp

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from flask import Flask
-from flask import url_for, request, render_template, json
+from flask import url_for, request, render_template, json, jsonify
 from flask import redirect, make_response, session, abort
-from data import db_session, news_api
+from data import db_session, news_api, news_resources
 from flask_login import LoginManager, login_user, login_required
 from flask_login import logout_user, current_user
+from flask_restful import reqparse, Api, Resource
+
+
 
 from data.users import User
 from data.news import News
@@ -14,6 +17,7 @@ from forms.news import NewsForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+api = Api(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
 
@@ -172,10 +176,17 @@ def session_test():
     return make_response(
         f"Вы пришли на эту страницу {visits_count + 1} раз")
 
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 def main():
     db_session.global_init("db/blogs.db")
     app.register_blueprint(news_api.blueprint)
+    # для списка объектов
+    api.add_resource(news_resources.NewsListResource, '/api/v2/news')
+    # для одного объекта
+    api.add_resource(news_resources.NewsResource, '/api/v2/news/<int:news_id>')
     app.run(port=8000, host='127.0.0.1')
 
 
